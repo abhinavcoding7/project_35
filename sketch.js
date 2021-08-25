b5fdffec76ca0fe96a81be27b7f014fc8d2eb3e2
abhinavcoding7 <89463952+abhinavcoding7@users.noreply.github.com>
2021-08-24 22:37:59 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 var balloon,balloonImage1,balloonImage2;
+var database;
+
 // create database and position variable here
 
 function preload(){
@@ -18,33 +20,51 @@ function setup() {
   balloon.addAnimation("hotAirBalloon",balloonImage1);
   balloon.scale=0.5;
 
-  textSize(20); 
+  database =firebase.database();
+
+var balloonPosition = database.ref("ballon/height");
+balloonPosition.on("value",readPosition);
+
+  
 }
+
+function readPosition(data){
+var position = data.val();
+
+balloon.x =position.x;
+balloon.y =position.y;
+
+}
+
 
 // function to display UI
-function draw() {
-  background(bg);
+function draw(){
+    background(bg);
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+  
+  
+    drawSprites();
 
-  if(keyDown(LEFT_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in left direction
-  }
-  else if(keyDown(RIGHT_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in right direction
-  }
-  else if(keyDown(UP_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in up direction
-  }
-  else if(keyDown(DOWN_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in down direction
-  }
-
-  drawSprites();
-  fill(0);
-  stroke("white");
-  textSize(25);
-  text("**Use arrow keys to move Hot Air Balloon!",40,40);
 }
+
+function changePosition(x,y){
+  balloon.x = balloon.x + x;
+  balloon.y = balloon.y + y;
+ database.ref("ballon/height").set({
+  x:balloon.x,
+  y:balloon.y
+ });
+}
+ 
+ 
